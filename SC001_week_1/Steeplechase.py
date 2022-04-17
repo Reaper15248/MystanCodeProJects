d48aa51bf836736a3fe43f3284072c5f71df162a
Jimmy15248 <103838386+Reaper15248@users.noreply.github.com>
2022-04-17 21:21:14 +0800

    Add files via upload

---
+++ SC001_week_1/Steeplechase.py
@@ -0,0 +1,59 @@
+"""
+File: Steeplechase.py
+Name: TODO:
+---------------------------------
+TODO:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel crosses hurdles in a 12x12 world
+    with a for loop 
+    """
+    for i in range(11):
+        if front_is_clear():
+            put_beeper()
+            put_beeper()
+            move()
+        else:
+            put_beeper()
+            jump()
+
+
+def jump():
+    up()
+    cross()
+    down()
+
+
+def up():
+    turn_left()
+    while not right_is_clear():
+        move()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def cross():
+    turn_right()
+    move()
+    turn_right()
+
+
+def down():
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
